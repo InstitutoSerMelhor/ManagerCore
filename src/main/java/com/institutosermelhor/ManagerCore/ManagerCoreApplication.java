c41d1c405ff9b263bf5cf612e678d1f7bd0c8819
Abner <abnerferreira84@gmail.com>
2023-11-06 15:29:42 -0300

    [start, config]: Setup inicial do projeto.

---
+++ src/main/java/com/institutosermelhor/ManagerCore/ManagerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.institutosermelhor.ManagerCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institutosermelhor/ManagerCore/ManagerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institutosermelhor.ManagerCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
