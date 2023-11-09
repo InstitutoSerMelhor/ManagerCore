ac2d52db342a1d7bab809c7ad24388b0dd80cbf2
Rafael Santos <rafaelcs_94@hotmail.com>
2023-11-09 16:09:51 -0300

    feat: Create Person service

---
+++ src/main/java/com/institutosermelhor/ManagerCore/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.institutosermelhor.ManagerCore.service;
+
+import com.institutosermelhor.ManagerCore.models.entity.Person;
+import com.institutosermelhor.ManagerCore.models.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+  PersonRepository repository;
+
+  @Autowired
+  public PersonService(PersonRepository repository) {
+    this.repository = repository;
+  }
+
+  public Person create(Person person) {
+    String hashedPassword = new BCryptPasswordEncoder().encode(person.getPassword());
+    person.setPassword(hashedPassword);
+    return repository.save(person);
+  }
+}
