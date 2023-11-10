fe6842e3813f00c8795cb1044e25bc5106d03152
InstitutoSerMelhor <devwebsermelhor@gmail.com>
2023-11-10 18:37:42 -0300

    feat: Agora o campo createdAt e updatedAt são atualizados automaticamente usando a biblioteca joda-time e algumas configurações no código.

--- src/main/java/com/institutosermelhor/ManagerCore/models/entity/Person.java
+++ src/main/java/com/institutosermelhor/ManagerCore/models/entity/Person.java
@@ -1,14 +1,20 @@
 package com.institutosermelhor.ManagerCore.models.entity;
 
-import com.institutosermelhor.ManagerCore.security.Role;
 import java.util.Date;
-import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import com.institutosermelhor.ManagerCore.security.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
 @Data
+@Builder
 @Document
+@AllArgsConstructor
 public class Person {
 
   @Id
@@ -23,19 +29,13 @@ public class Person {
 
   private Role role;
 
+  @CreatedDate
   private Date createdAt;
 
+  @LastModifiedDate
   private Date updatedAt;
 
+  @Builder.Default
   private boolean isDeleted = false;
 
-  public Person(String username, String email, String password, Role role, Date createdAt,
-      Date updatedAt) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.role = role;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-  }
 }
---
+++ src/main/java/com/institutosermelhor/ManagerCore/util/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.institutosermelhor.ManagerCore.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
