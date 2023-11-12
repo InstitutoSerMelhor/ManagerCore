7ff282bd450f4b21ab75b731df9136662a08a7f9
Rafael Santos <rafaelcs_94@hotmail.com>
2023-11-12 11:06:14 -0300

    build: Config swagger setup

--- src/main/java/com/institutosermelhor/ManagerCore/ManagerCoreApplication.java
+++ src/main/java/com/institutosermelhor/ManagerCore/ManagerCoreApplication.java
@@ -1,13 +1,20 @@
 package com.institutosermelhor.ManagerCore;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+    description = "OpenApi documentation for Instituto Ser Melhor API",
+    title = "Instituto Ser Melhor API",
+    version = "1.0"
+))
 public class ManagerCoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManagerCoreApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ManagerCoreApplication.class, args);
+  }
 
 }
