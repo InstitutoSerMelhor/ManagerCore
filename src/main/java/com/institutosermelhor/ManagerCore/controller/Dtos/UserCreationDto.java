2f11cd828bbec166aace432512d2fa97c5251218
Rafael Santos <rafaelcs_94@hotmail.com>
2023-11-13 10:03:09 -0300

    ref: Add toEntity method for UserCreationDto

--- src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/UserCreationDto.java
+++ src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/UserCreationDto.java
@@ -1,5 +1,11 @@
 package com.institutosermelhor.ManagerCore.controller.Dtos;
 
+import com.institutosermelhor.ManagerCore.models.entity.User;
+
 public record UserCreationDto(String username, String email, String password) {
 
+  public User toEntity() {
+    return User.builder().username(username).email(email)
+        .password(password).build();
+  }
 }
