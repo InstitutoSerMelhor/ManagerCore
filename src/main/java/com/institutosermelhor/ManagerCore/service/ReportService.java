3111936a6be83bb2ec6277dc24a22762d0c18fa6
Rafael Santos <rafaelcs_94@hotmail.com>
2023-11-14 11:49:39 -0300

    feat: implements GridFs for report file storage

--- src/main/java/com/institutosermelhor/ManagerCore/service/ReportService.java
+++ src/main/java/com/institutosermelhor/ManagerCore/service/ReportService.java
@@ -1,49 +1,92 @@
 package com.institutosermelhor.ManagerCore.service;
 
+import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDownloadDto;
+import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDto;
+import com.institutosermelhor.ManagerCore.infra.exception.BadRequestException;
+import com.institutosermelhor.ManagerCore.infra.exception.ConflictException;
 import com.institutosermelhor.ManagerCore.infra.exception.NotFoundException;
 import com.institutosermelhor.ManagerCore.models.entity.Report;
 import com.institutosermelhor.ManagerCore.models.repository.ReportRepository;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-@Tag(name = "Reports")
 public class ReportService {
+
+  private final GridFsTemplate gridFsTemplate;
+
+  private final GridFsOperations gridFsOperations;
 
   private final ReportRepository repository;
 
   @Autowired
-  public ReportService(ReportRepository repository) {
+  public ReportService(GridFsTemplate gridFsTemplate,
+      GridFsOperations gridFsOperations, ReportRepository repository) {
+    this.gridFsTemplate = gridFsTemplate;
+    this.gridFsOperations = gridFsOperations;
     this.repository = repository;
   }
 
-  public Report save(MultipartFile file) throws Exception {
-    String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-
-    long maxSize = 16000000; //16mb
-    if (file.getSize() >= maxSize) {
-      throw new Exception("File size too large");
-    }
-
+  public String saveFile(MultipartFile file) throws IOException {
     if (!Objects.equals(file.getContentType(), "application/pdf")) {
-      throw new Exception("File type not supported");
+      throw new BadRequestException("File type not supported");
     }
 
-    try {
-      Report report = Report.builder().name(fileName).type(file.getContentType())
-          .data(file.getBytes())
-          .build();
-      return repository.save(report);
-    } catch (Exception e) {
-      throw new Exception("Could not save file " + fileName);
+    if (repository.findByFileName(file.getOriginalFilename()) != null) {
+      throw new ConflictException("File name already exists");
     }
+
+    DBObject metadata = new BasicDBObject();
+    metadata.put("fileSize", file.getSize());
+
+    Object fileID = gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename(),
+        file.getContentType(), metadata);
+
+    Report report = Report.builder().id(fileID.toString()).fileName(file.getOriginalFilename())
+        .fileSize(file.getSize()).build();
+    repository.save(report);
+
+    return fileID.toString();
   }
 
-  public Report getFile(String id) {
-    return repository.findById(id).orElseThrow(() -> new NotFoundException("File not found!"));
+
+  public ReportDownloadDto getFileById(String id) throws IOException {
+    Report report = repository.findById(id)
+        .orElseThrow(() -> new NotFoundException("File not found"));
+
+    GridFSFile gridFSFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
+
+    if (!report.isEnabled() || gridFSFile == null || gridFSFile.getMetadata() == null) {
+      throw new NotFoundException("File not found!");
+    }
+
+    return new ReportDownloadDto(gridFSFile.getFilename(),
+        gridFSFile.getMetadata().get("_contentType").toString(),
+        gridFSFile.getMetadata().get("fileSize").toString(),
+        IOUtils.toByteArray(gridFsOperations.getResource(gridFSFile).getInputStream()));
+  }
+
+  public List<Report> getFiles() {
+    return repository.findByIsEnabledTrue();
+  }
+
+  public void deleteFile(String id) {
+    Report report = repository.findById(id)
+        .orElseThrow(() -> new NotFoundException("File not found"));
+    report.setEnabled(false);
+    repository.save(report);
   }
 }
