ffd3455fa67733b46a764462d27c4024ad9bf96b
abnerferreiradesousa <abnerferreira84@gmail.com>
2023-11-16 10:54:23 -0300

    feat: Add rota de atualização de relatórios.

--- src/main/java/com/institutosermelhor/ManagerCore/controller/ReportController.java
+++ src/main/java/com/institutosermelhor/ManagerCore/controller/ReportController.java
@@ -1,10 +1,5 @@
 package com.institutosermelhor.ManagerCore.controller;
 
-import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDownloadDto;
-import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDto;
-import com.institutosermelhor.ManagerCore.models.entity.Report;
-import com.institutosermelhor.ManagerCore.service.ReportService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +12,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDownloadDto;
+import com.institutosermelhor.ManagerCore.controller.Dtos.ReportDto;
+import com.institutosermelhor.ManagerCore.models.entity.Report;
+import com.institutosermelhor.ManagerCore.service.ReportService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/reports")
@@ -44,10 +45,8 @@ public class ReportController {
   @GetMapping("/{id}")
   public ResponseEntity<ByteArrayResource> getFileById(@PathVariable String id) throws IOException {
     ReportDownloadDto report = service.getFileById(id);
-    return ResponseEntity.ok()
-        .contentType(MediaType.parseMediaType(report.type()))
-        .header(HttpHeaders.CONTENT_DISPOSITION,
-            "attachment; filename=\"" + report.name() + "\"")
+    return ResponseEntity.ok().contentType(MediaType.parseMediaType(report.type()))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + report.name() + "\"")
         .body(new ByteArrayResource(report.data()));
   }
 
@@ -58,6 +57,13 @@ public class ReportController {
         .map(report -> new ReportDto(report.getId(), report.getFileName(), report.getFileSize()))
         .toList();
     return ResponseEntity.ok(reportsDto);
+  }
+
+  @Secured("ADMIN")
+  @PutMapping()
+  public ResponseEntity<String> updateFile(@RequestParam MultipartFile file) throws Exception {
+    String fileId = service.updateFile(file);
+    return ResponseEntity.ok().body(fileId);
   }
 
   @Secured("ADMIN")
