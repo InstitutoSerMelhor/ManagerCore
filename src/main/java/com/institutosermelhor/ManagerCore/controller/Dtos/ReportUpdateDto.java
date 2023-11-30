a94ec9ac6e6b07f82f796b2fb82fb9076f69f370
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-11-30 10:45:49 -0300

    feat: add some validate in report update dto

--- src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/ReportUpdateDto.java
+++ src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/ReportUpdateDto.java
@@ -2,6 +2,15 @@ package com.institutosermelhor.ManagerCore.controller.Dtos;
 
 import com.institutosermelhor.ManagerCore.util.ReportType;
 
-public record ReportUpdateDto(String name, ReportType reportType) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record ReportUpdateDto(
+    @NotNull(message = "Name must be provided")
+    @NotEmpty(message = "Name cannot be empty")
+    String name, 
+    @NotNull(message = "Name must be provided")
+    @NotEmpty(message = "Name cannot be empty")
+    ReportType reportType) {
 
 }
