97af8da56b51d6a3f98d3c574b827b595a41f43e
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-12-04 13:48:55 -0300

    feat: Add the last validation Report

--- src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/ReportUpdateDto.java
+++ src/main/java/com/institutosermelhor/ManagerCore/controller/Dtos/ReportUpdateDto.java
@@ -5,12 +5,14 @@ import com.institutosermelhor.ManagerCore.util.ReportType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ReportUpdateDto(
     @NotNull(message = "Name must be provided")
     @NotEmpty(message = "Name cannot be empty")
     @Min(value=3, message = "Name must have at least 3 characteres")
     String name,
+    @Size(min=6, max=8, message = "Report type must be between 6 and 8 characteres. Values accepted: ACTIVITY, BALANCE, BYLAWS and PROJECT.")
     ReportType reportType) {
 
 }
