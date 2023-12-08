b1f431dde02f021a244cb6ac1b600c74af19e725
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-12-08 15:28:02 -0300

    feat: add new test for Role USER block acess and fix erro in tests with meth wrong (Mockit.verigy)

--- src/test/java/com/institutosermelhor/ManagerCore/integration/AuthenticationControllerTest.java
+++ src/test/java/com/institutosermelhor/ManagerCore/integration/AuthenticationControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +20,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.institutosermelhor.ManagerCore.MongoDbTestcontainerConfigTest;
 import com.institutosermelhor.ManagerCore.controller.Dtos.UserCreationDto;
+import com.institutosermelhor.ManagerCore.infra.security.Role;
 import com.institutosermelhor.ManagerCore.models.entity.User;
 import com.institutosermelhor.ManagerCore.models.repository.UserRepository;
 import com.institutosermelhor.ManagerCore.service.UserService;
@@ -54,32 +54,28 @@ class AuthenticationControllerTest extends MongoDbTestcontainerConfigTest {
     @DisplayName("Test if Admin is sigin up in aplication")
     @WithMockUser(authorities = {"ADMIN"})
     void testSaveAdmin() throws Exception{
-        UserCreationDto newUserAdmin = UserCreationDto.builder().name("new name").email("new.email@gmail.com").password("newPass123@").build();
+        UserCreationDto newUserAdmin = new UserCreationDto("New Name", "new.email@gmail.com", "NewPass123@");
 
         // Verify end point user admin:
         mockMvc.perform(
             MockMvcRequestBuilders.post("/register/admin")
             .contentType(MediaType.APPLICATION_JSON)
             .content(new ObjectMapper().writeValueAsString(newUserAdmin)))
-        .andExpect(status().isCreated())
-        // .andExpect(jsonPath("$").value(newUserAdmin))
-        .andExpect(jsonPath("$").isNotEmpty())
-        .andExpect(jsonPath("$.id").isNotEmpty())
-        .andExpect(jsonPath("$.name").isNotEmpty())
-        .andExpect(jsonPath("$.email").isNotEmpty())
-        .andExpect(jsonPath("$.role").isNotEmpty())
-        .andReturn();
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$").isNotEmpty())
+            .andExpect(jsonPath("$.id").isNotEmpty())
+            .andExpect(jsonPath("$.name").isNotEmpty())
+            .andExpect(jsonPath("$.email").isNotEmpty())
+            .andExpect(jsonPath("$.role").isNotEmpty())
+            .andReturn();
         
-        Mockito.verify(userService).saveAdmin(newUserAdmin.toEntity());
-
         //Verify if user admin is registered correcly:
         User userCreated = userRepository.findAll().get(0);
 
-        System.out.println("testando" + userCreated);
-
-        // Assertions.assertEquals("ROLE", userCreated.getRole());
+        Assertions.assertNotNull(userCreated.getId());
+        Assertions.assertEquals(Role.ADMIN, userCreated.getRole());
         Assertions.assertEquals(true, encoder.matches("NewPass123@", userCreated.getPassword()));
-        Assertions.assertEquals("new name", userCreated.getName());
+        Assertions.assertEquals("New Name", userCreated.getName());
         Assertions.assertEquals("new.email@gmail.com", userCreated.getEmail());
     }
 
@@ -95,8 +91,27 @@ class AuthenticationControllerTest extends MongoDbTestcontainerConfigTest {
             MockMvcRequestBuilders.post("/register/admin")
             .contentType(MediaType.APPLICATION_JSON)
             .content(new ObjectMapper().writeValueAsString(newUserAdmin)))
-        .andExpect(status().is4xxClientError())
-        .andExpect(jsonPath("$.message").isNotEmpty())
-        .andExpect(jsonPath("$.message").value(is("Email already registered!")));
+            .andExpect(status().is4xxClientError())
+            .andExpect(jsonPath("$.message").isNotEmpty())
+            .andExpect(jsonPath("$.message").value(is("Email already registered!")));
+    }
+
+    @Test
+    @DisplayName("Test if normal user try use endpoint save admin")
+    @WithMockUser(authorities = {"USER"})
+    void testBlockUserRole() throws Exception{
+        UserCreationDto newUserAdmin = new UserCreationDto("New Name", "new.email@gmail.com", "NewPass123@");
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/register/admin")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(newUserAdmin)))
+            .andExpect(status().is4xxClientError())
+            .andExpect(header().string("X-Content-Type-Options", "nosniff"))
+            .andExpect(header().string("X-Frame-Options", "DENY"))
+            .andExpect(header().string("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate"))
+            .andExpect(header().string("Pragma", "no-cache"))
+            .andExpect(header().string("Expires", "0"))
+            .andExpect(jsonPath("$").value(is("Acesso negado")));
     }
 }
